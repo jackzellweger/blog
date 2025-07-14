c53767f4ccd40098e968b597a78788611b92ddb4
Jack Zellweger <jack.zellweger@hudsonts.com>
2025-07-14 19:26:21 -0400

    cleanup

--- server.py
+++ server.py
@@ -226,7 +226,7 @@ window.MathJax = {{
             # Check if babe.png exists
             image_html = ""
             if os.path.exists('./images/babe.png'):
-                image_html = '<center><img src="/babe.png" alt="Babe"></center>'
+                image_html = '<center><img src="./images/babe.png" alt="Babe"></center>'
             
             # Generate complete HTML page
             html_content = f"""
